44648b705466b2e1bdd22d95b7bc900e76e47c3a
Csávás Gergely <csavas.g95@gmail.com>
2024-12-16 19:34:58 +0100

    MovieListViewModel feltöltése az alapvető funkciókkal, átadása a MovieListScreen funkciónak

--- app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListScreen.kt
+++ app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListScreen.kt
@@ -2,9 +2,11 @@ package com.example.ccompmovielist.ui.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 
 @Composable
-fun MovieListScreen(navController: NavHostController) {
+fun MovieListScreen(movieListViewModel: MovieListViewModel = viewModel(),
+                    navController: NavHostController) {
     Text(text = "Movie list")
 }
--- app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListViewModel.kt
+++ app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListViewModel.kt
@@ -1,2 +1,33 @@
 package com.example.ccompmovielist.ui.screen
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import com.example.ccompmovielist.ui.data.MovieItem
+
+class MovieListViewModel : ViewModel() {
+    private val _movieList = mutableStateListOf<MovieItem>()
+
+    fun getAllMovies(): List<MovieItem> {
+        return _movieList
+    }
+    fun addMovie(movie: MovieItem) {
+        _movieList.add(movie)
+    }
+    fun removeMovie(movie: MovieItem) {
+        _movieList.remove(movie)
+    }
+    fun changeWatchedStatus(movie: MovieItem, watched: Boolean) {
+        val index = _movieList.indexOf(movie)
+
+        val newMovie = movie.copy(
+            title = movie.title,
+            description = movie.description,
+            releaseDate = movie.releaseDate,
+            note = movie.note,
+            genre = movie.genre,
+            watched = watched
+        )
+
+        _movieList[index] = newMovie
+    }
+}
