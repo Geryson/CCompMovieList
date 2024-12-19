2a67fa4cce135679d0c6071aa962cfcac2b52f1d
Csávás Gergely <csavas.g95@gmail.com>
2024-12-19 15:41:45 +0100

    Új film hozzáadásához kötött címmező és gomb elhelyezve a MovieListScreen felületre és bekötve

--- app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListScreen.kt
+++ app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,6 +15,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
@@ -21,17 +23,25 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.ccompmovielist.ui.data.MovieGenre
 import com.example.ccompmovielist.ui.data.MovieItem
+import java.util.Date
+import java.util.UUID
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,6 +66,30 @@ fun MovieListScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
+                var newMovieTitle by rememberSaveable { mutableStateOf("") }
+                OutlinedTextField(
+                    value = newMovieTitle,
+                    onValueChange = { newMovieTitle = it },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Row {
+                    Button(onClick = {
+                        movieListViewModel.addMovie(
+                            MovieItem(
+                                id = UUID.randomUUID().toString(),
+                                title = newMovieTitle,
+                                description = "desc",
+                                releaseDate = Date(System.currentTimeMillis()).toString(),
+                                note = "notes",
+                                genre = MovieGenre.ACTION,
+                                watched = false
+                            )
+                        )
+                    }) {
+                        Text(text = "Add")
+                    }
+                }
+
                 if (movieListViewModel.getAllMovies().isEmpty()) {
                     Text(text = "No movies to show")
                 } else {
