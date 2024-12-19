cd363e3517ed68e66055cab1e6c6937a93787614
Csávás Gergely <csavas.g95@gmail.com>
2024-12-19 14:52:47 +0100

    MovieCard létrehozása, MovieListScreen átalakítása Scaffold alapok segítségével

--- app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListScreen.kt
+++ app/src/main/java/com/example/ccompmovielist/ui/screen/MovieListScreen.kt
@@ -1,12 +1,89 @@
 package com.example.ccompmovielist.ui.screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.ccompmovielist.ui.data.MovieItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieListScreen(movieListViewModel: MovieListViewModel = viewModel(),
                     navController: NavHostController) {
-    Text(text = "Movie list")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Movie List") },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+                    }
+                }
+            )
+        },
+        content = {
+            innerPadding ->
+                Column(modifier = Modifier.padding(innerPadding),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center) {
+                    Text(text = "Content")
+                }
+        }
+    )
+}
+
+@Composable
+fun MovieCard(movie: MovieItem,
+              onMovieCheckChange: (Boolean) -> Unit = {},
+              onRemoveMovie: () -> Unit = {}) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        modifier = Modifier.padding(5.dp)
+    ) {
+        Row(modifier = Modifier.padding(20.dp)) {
+            Text(movie.title)
+            Spacer(modifier = Modifier.fillMaxSize(0.7f))
+            Checkbox(
+                checked = movie.watched,
+                onCheckedChange = onMovieCheckChange
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete",
+                modifier = Modifier.clickable { onRemoveMovie() },
+                tint = Color.Red
+            )
+        }
+    }
 }
